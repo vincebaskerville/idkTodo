3c3c3f30b11f0b95efec184e74bc9aa3b7d6c21b
Vince Baskerville <vince.baskerville@gmail.com>
2012-11-27 18:18:44 -0500

    ...

--- inc/db_fns.php
+++ inc/db_fns.php
@@ -1,7 +1,7 @@
 <?php
 
 function db_connect() {
-   $result = new mysqli('external-db.s69392.gridserver.com', 'db69392_idktodo', 'happyness', 'db69392_idktodo');
+   $result = new mysqli('server.com', 'name', 'pw', 'title');
    if (!$result) {
      throw new Exception('Could not connect to database server');
    } else {
